f65640f29be3db0b053d95ee443a739906142582
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-03-09 23:38:39 +0200

    return single tour component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
+
+  const fetchTours = async () => {
+    setLoading(true)
+    
+    try {
+      const response = await fetch(url)
+      const toursData = await response.json()
+      setLoading(false)
+      setTours(toursData)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
   if(loading) {
     return (
       <main>
@@ -14,7 +33,9 @@ function App() {
       </main>
     )
   }
-  return <h2>Tours Project Setup</h2>
+  return <main>
+    <Tours tours={tours} />
+  </main>
 }
 
 export default App
